64ee2a60a5e69eab3242ec1219c343cbedb08211
Wangari-j <wangarijudy@gmail.com>
2024-02-21 22:59:33 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,20 @@
+#return statements
+
+def findArea(num):
+ result = num * num
+ return result
+
+square = (findArea(5))
+square1 = (findArea(12))
+square2 = (findArea(40))
+
+print(square)
+print(square1)
+print(square2)
+
+#the pass statement
+def wangariJ():
+    pass
+
+wangariJ() #this executes without error
+
